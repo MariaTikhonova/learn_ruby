4e05cfcec0ed5bdfb93eda9ee327126d19712d3a
MariaTikhonova <ritz.rkraft@gmail.com>
2017-10-05 16:18:57 +0300

    temperature.rb passed RSpec tests

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,19 @@
 #write your code here
+def ftoc(num)
+	((num.to_i-31)/1.8).to_i
+end
+
+ftoc(32)
+ftoc(212)
+ftoc(98.6)
+ftoc(68)
+
+def ctof(num)
+	(num*1.8)+32
+end
+
+ctof(0)
+ctof(100)
+ctof(20)
+ctof(37)
+
