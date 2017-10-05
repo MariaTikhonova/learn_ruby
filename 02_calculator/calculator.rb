28b028e8c24c0c9a585988a6b22208ed5a922f93
MariaTikhonova <ritz.rkraft@gmail.com>
2017-10-05 16:20:17 +0300

    calculator.rb passed RSpec tests

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
-#write your code here
+def add(a,b) #write your code here
+a+b
+end
+
+add(0,0)
+add(2,2)
+add(2,6)
+
+def subtract(a,b)
+a-b
+end
+
+subtract(10,4)
+
+def sum(ar)
+return 0 if ar.empty? 
+ar.inject(:+)
+end
+
+sum([])
+sum([7])
+sum([7,11])
+sum([1,3,5,7,9])
