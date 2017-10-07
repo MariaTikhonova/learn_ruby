3b2da57c9305dc3342b326b43571281c1a9d0956
MariaTikhonova <ritz.rkraft@gmail.com>
2017-10-07 15:28:52 +0300

    passes rspec tests

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,20 @@
 class Timer
+
+attr_accessor :seconds
+
+def initialize
+	@seconds=0
+end
+
+def seconds
+	@seconds
+
+end
+
+def time_string
+	return "00:00:00" if @seconds == 0
+		@time_string= Time.at(@seconds).utc.strftime("%H:%M:%S")
+
+	end
   #write your code here
 end
